cbd3a129e11e2dbebd59318213fb84b1f464d532
Tyler Wolf <tyler@palominolabs.com>
2012-06-01 12:11:00 -0700

    Workaround for a potential bug in Sencha Touch, where new records being created send the Sencha Touch-created ID to the server

--- app/data/proxy/StackMob.js
+++ app/data/proxy/StackMob.js
@@ -2,11 +2,17 @@ Ext.define('StackMobDemo.data.proxy.StackMob', {
     extend: 'Ext.data.proxy.Rest',
     alias: 'proxy.stackmob',
 
+    requires: ['StackMobDemo.data.writer.StackMob'],
+
     config: {
         headers: {
             'Accept': 'application/vnd.stackmob+json; version=0',
             'X-StackMob-Proxy': 'stackmob-api',
             'X-StackMob-User-Agent': 'StackMob (JS; 0.2.1)/stackmobdemo'
+        },
+
+        writer: {
+            type: 'stackmob'
         },
 
         // Disable cache busting / paging for now, since StackMob rejects the GET params added by ServerProxy.
